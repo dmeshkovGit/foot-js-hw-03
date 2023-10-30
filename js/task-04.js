67a5aa47eebf281b54fd10ed3be7d6a0ac50826e
dmitriy-meshkov <140300183+dmeshkovGit@users.noreply.github.com>
2023-10-30 23:15:42 +0200

    Initial commit

---
+++ js/task-04.js
@@ -0,0 +1,32 @@
+function getShippingCost(country) {
+    let deliveryPrice
+    switch (country) {
+        case "China":
+         deliveryPrice = 100   
+            break;
+     case "Chile":
+         deliveryPrice = 250   
+            break;
+     case "Australia":
+          deliveryPrice = 170  
+            break;
+     case "Jamaica":
+          deliveryPrice = 120  
+            break;
+        default:
+            deliveryPrice = "no delivery"
+            break;
+    }
+    if (deliveryPrice === "no delivery") {
+        return "Sorry, there is no delivery to your country"
+    } else {
+        return `Shipping to ${country} will cost ${deliveryPrice} credits`
+    }
+}
+
+console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
+console.log(getShippingCost("Germany")); // "Sorry, there is no delivery to your country"
+console.log(getShippingCost("China")); // "Shipping to China will cost 100 credits"
+console.log(getShippingCost("Chile")); // "Shipping to Chile will cost 250 credits"
+console.log(getShippingCost("Jamaica")); // "Shipping to Jamaica will cost 120 credits"
+console.log(getShippingCost("Sweden")); // "Sorry, there is no delivery to your country"
